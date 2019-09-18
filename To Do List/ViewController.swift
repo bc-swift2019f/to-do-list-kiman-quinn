162eb94b1fa4fb9dcaa05f0a0f2308a8123cad6d
Kim-An Quinn <kiman.quinn@bc.edu>
2019-09-18 15:11:06 -0400

    5.3 completed: implemented cancel, save, and + buttons

--- To Do List/ViewController.swift
+++ To Do List/ViewController.swift
@@ -19,7 +19,32 @@ class ViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
     }
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "editItem"{
+            let destination = segue.destination as! DetailViewController
+            let index = tableView.indexPathForSelectedRow!.row
+            destination.toDoItem = toDoArray[index]
+        }else{
+            if let selectedPath = tableView.indexPathForSelectedRow{
+                tableView.deselectRow(at: selectedPath, animated: false)
+            }
+        }
+        
+    }
+    
+    @IBAction func unwindFromDetailViewController(segue: UIStoryboardSegue){
+        
+        let sourceViewController = segue.source as! DetailViewController
+        if let indexPath = tableView.indexPathForSelectedRow{
+            toDoArray[indexPath.row] = sourceViewController.toDoItem!
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }else{
+            let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
+            toDoArray.append(sourceViewController.toDoItem!)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        }
+        
+    }
 
 }
 
